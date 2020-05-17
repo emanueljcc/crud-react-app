8420286c93af399a354233f647838ccdaa3c3075
Emanuel Castillo <emanuel.castillo@agence.cl>
2020-05-17 04:24:42 -0400

    Fixed - minor CSS setting

--- src/components/NotData.js
+++ src/components/NotData.js
@@ -3,7 +3,7 @@ import { Paper, Typography } from '@material-ui/core';
 
 export default function NotData() {
     return (
-        <Paper elevation={0} style={{ padding: 100, textAlign: "center" }}>
+        <Paper elevation={0} style={{ padding: 100, textAlign: "center", marginTop: 50 }}>
             <Typography variant="h5" gutterBottom>
                 No data available.
             </Typography>
